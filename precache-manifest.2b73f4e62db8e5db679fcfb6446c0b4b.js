717a28f54c99c9837f693df5cce87de2a95eedb9
Mohammad <57011800+Mammad10@users.noreply.github.com>
2021-06-26 10:11:56 +0300

    Updates

---
+++ precache-manifest.2b73f4e62db8e5db679fcfb6446c0b4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f864dc9524fec368fbe8f183e78b9121",
+    "url": "/reviewers/index.html"
+  },
+  {
+    "revision": "1e5ef09910f58d091f6c",
+    "url": "/reviewers/static/css/main.e64ab053.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/reviewers/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reviewers/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5ef09910f58d091f6c",
+    "url": "/reviewers/static/js/main.88dcc022.chunk.js"
+  },
+  {
+    "revision": "a69562c09469840bc0b5",
+    "url": "/reviewers/static/js/runtime-main.2246d755.js"
+  }
+]);
